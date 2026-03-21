2de01a59c0422edee9725ac4b5020ea614a5f630
dpk <dpk@gmail.com>
2026-03-21 11:06:03 +0530

    Fix: convert api/chat.js to ESM for Vercel compatibility

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,6 @@
-const axios = require('axios');
+import axios from 'axios';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
@@ -57,4 +57,4 @@ module.exports = async (req, res) => {
             details: error.response?.data?.error?.message || error.response?.data?.error || error.message
         });
     }
-};
+}
